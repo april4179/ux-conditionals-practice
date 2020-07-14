0f7736d10bd0f0f12adbfaf3c68a9b723fcd762f
April Chilldres <aprilchilldres@Aprils-MacBook-Air.local>
2020-07-14 00:22:27 -0500

    first commit

--- conditionals.js
+++ conditionals.js
@@ -14,10 +14,14 @@ let number = 5;
 
 // 1. Create an if statement where the number is greater than 10.
 // If it is, then log "It is bigger than 10!" using console.log().
-
+if (number > 10) {
+  console.log("It is bigger than 10!");
+}
 // 2. Change the number variable to something larger than 10, run your code and
 // make sure it works.
-
+else {
+  console.log('It is 10 or greater!');
+}
 /*
 ---------------------------------------------------------------------------
 Exercise Two
@@ -25,17 +29,25 @@ Exercise Two
 Determining a color
 */
 
-let color = "";
+let color = blue;
 // 1. Pick either "blue", "red", or "green" and assign it to the color variable.
 
 // 2. Create an if statement, if the color is "blue", then log "The sky is blue." using console.log
-
+if (color = blue) {
+  console.log("The sky is blue.")
+}
 // 3. Create an else if statement, and if the color is "red" then log "The flower is red."
-
+else if (color = red) {
+  console.log('The flower is red.')
+}
 // 4. create another else if statement where if the color is "green" then log "The Leaf is green."
-
+else if (color = green) {
+  console.log('The leaf is green.')
+}
 // 5. Finally, create an else statement and log "It is a different color".
-
+else {
+  console.log('It is a different color.')
+}
 // 6. Test your code! Change the value of the color variable to different colors and run your code.
 // The correct message should log to the console.
 
@@ -55,8 +67,19 @@ A good practice is to take the most restrictive condition and check it first (li
 then work your way to the most broad condition ("Some other number").
 
 Hint Two: You can either do this with nested if statements, or you can do it with 
-a chain of if else statements, some of those just might need multible conditions (like: if (a && b) )
+a chain of if else statements, some of those just might need multiple conditions (like: if (a && b) )
 */
 
 let number = 5;
 // Your code here.
+const answer = number > 10 ? "greater than 10" : number < 20 ?  "Between 10 and 20!"
+
+if (number >= 20) {
+  console.log('20 or greater!')
+}
+if (number = 16) {
+  console.log('16!')
+}
+if (number <= 10) {
+  console.log('10 or less!')
+}
